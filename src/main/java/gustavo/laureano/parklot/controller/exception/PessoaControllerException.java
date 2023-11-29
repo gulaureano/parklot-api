90de3b31d87eef9147b1c70fbd51399cfd2a5e1f
Gustavo Laureano <gustavolaureano3510@gmail.com>
2023-11-29 14:38:01 -0300

    Modificando a classe de exception da Pessoa

--- src/main/java/gustavo/laureano/parklot/controller/exception/PessoaControllerException.java
+++ src/main/java/gustavo/laureano/parklot/controller/exception/PessoaControllerException.java
@@ -1,7 +1,5 @@
 package gustavo.laureano.parklot.controller.exception;
 
-import java.time.LocalDate;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
@@ -9,26 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import gustavo.laureano.parklot.dto.PessoaCadastradaDto;
 import gustavo.laureano.parklot.exception.PessoaExistenteException;
 
 @RestControllerAdvice
 public class PessoaControllerException extends Exception {
 	
 	@ExceptionHandler(PessoaExistenteException.class)
-	public ResponseEntity<PessoaCadastradaDto> pessoaExistente(PessoaExistenteException ex, HttpServletRequest request){
-		String nome =  (String) request.getAttribute("nome");
-		String cpf =  (String) request.getAttribute("cpf");
-		LocalDate data = (LocalDate) request.getAttribute("cpf");
-		
-		PessoaCadastradaDto dto = new PessoaCadastradaDto();
-		dto.setNome(nome);
-		dto.setCpf(cpf);
-		dto.setDataNascimento(data);
-		dto.setMensagemRetorno("O CPF digitado já está cadastrado");
-		
-		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(dto);
+	public ResponseEntity<String> pessoaExistente(PessoaExistenteException ex, HttpServletRequest request){
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("O CPF informado já está cadastrado!");
 		
 	}
 	
